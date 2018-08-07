318ea638ddbba1ff3423e11a80d3883161f67a99
Paul Elion <paul.j.elion@gmail.com>
2018-08-07 21:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  kittens.slice(0, kittens.length -1)
-  return kittens
+  return kittens.slice(0, kittens.length -1)
+
 }
 
 function removeFirstKitten(){
